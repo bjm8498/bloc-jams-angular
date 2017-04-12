4fb9049ac2723f6eadbae804a1b2b7211dcc04e0
Brian Moore <bjm8498@gmail.com>
2017-04-11 21:40:05 -0700

    Checkpoint 6 corrections needs addition of fixture

--- app/scripts/Controllers/CollectionCtrl.js
+++ app/scripts/Controllers/CollectionCtrl.js
@@ -2,7 +2,7 @@
     function CollectionCtrl (Fixtures) {
         this.albums = [];
         for( var i = 0; i < 12; i++) {
-            this.albums.push(Fixtures.getAlbum());
+            this.albums.push(angular.copy(Fixtures.getAlbum()));
         }
     }
     
